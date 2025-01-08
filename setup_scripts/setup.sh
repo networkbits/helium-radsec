b5d65c424d03b5dd8da67c541455b3bf637d8d35
networkbits <133474221+networkbits@users.noreply.github.com>
2025-01-08 01:46:09 -0600

    Update setup.sh
    Remove unnecessary docker components

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -7,27 +7,8 @@ sudo apt-get install -y \
     curl \
     software-properties-common 
 
-
-
 sudo apt-get -y install tree jq
 
-
 sudo usermod -aG docker $(whoami)
-
-echo "Docker Installed"
-if  ! sudo docker --version;
-then
-    echo "Failed to install Docker"
-    exit 1
-fi
-
-
-if  ! sudo docker info;
-then
-    echo "Docker Daemon is not Running"
-    echo "Starting Docker Daemon"
-    sudo systemctl start docker
-fi
-
 
 echo "Installation Completed"
